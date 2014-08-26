e19ece3451b02dceb53c7090c63760357020900f
Niek Haarman <niek@label305.com>
2014-08-26 15:04:06 +0200

    Project setup

---
+++ src/Label305/AujaLaravel/AujaLaravelServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Label305\AujaLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class AujaLaravelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
