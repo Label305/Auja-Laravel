49981a81e55a310d3c0452a787aa8fad56e870cf
Niek Haarman <niek@label305.com>
2014-08-29 14:58:28 +0200

    Fixed name

--- src/Label305/AujaLaravel/AujaServiceProvider.php
+++ src/Label305/AujaLaravel/AujaServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class AujaLaravelServiceProvider extends ServiceProvider {
+class AujaServiceProvider extends ServiceProvider {
 
     /**
      * Indicates if loading of the provider is deferred.
