e0bd1052006d2d64c3751ae4a21684b4f5447a38
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-12-16 13:11:15 +0100

    Update index.blade.php

--- src/views/admin/index.blade.php
+++ src/views/admin/index.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         {{ HTML::style('css/vendor/auja.css') }}
         {{ HTML::style('css/vendor/trumbowyg.css') }}
         {{ HTML::style('css/vendor/ionicons.css') }}
